c5c2f88d817dbe5ec62d65e32a7d3cba04cff1e0
NataliaJanda <jkl.natalia1989@gmail.com>
2024-01-25 22:42:11 +0100

    init

---
+++ serwer/src/main/java/com/Sheltersapp/Sheltersapp/SheltersappApplication.java
@@ -0,0 +1,13 @@
+package com.Sheltersapp.Sheltersapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SheltersappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SheltersappApplication.class, args);
+	}
+
+}
---
+++ serwer/src/test/java/com/Sheltersapp/Sheltersapp/SheltersappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Sheltersapp.Sheltersapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SheltersappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
