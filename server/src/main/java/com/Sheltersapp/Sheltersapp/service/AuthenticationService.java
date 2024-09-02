3ed24dec75ad9888f53fcb858c6ad276faebe35c
NataliaJanda <jkl.natalia1989@gmail.com>
2024-09-03 01:37:23 +0200

    Register and Authentication with JwtToken and email sender

--- server/src/main/java/com/Sheltersapp/Sheltersapp/service/AuthenticationService.java
+++ server/src/main/java/com/Sheltersapp/Sheltersapp/service/AuthenticationService.java
@@ -96,7 +96,7 @@ public class AuthenticationService {
         }
     }
 
-    private void sendVerificationEmail(Users users) { //TODO: Update with company logo
+    private void sendVerificationEmail(Users users) {
         String subject = "Account Verification";
         String verificationCode = "VERIFICATION CODE " + users.getVerificationCode();
         String htmlMessage = "<html>"
